5044bd35986db601307f74d3be6bc0a083d88b5b
wangjiahao <1208200303@qq.com>
2020-12-16 09:07:37 +0800

    增加了站内消息

--- src/components/innerCenter/index.vue
+++ src/components/innerCenter/index.vue
@@ -1,13 +1,50 @@
 <template>
-    $END$
+    <div>
+        <div class="btnArr">
+            <div class="btnInnerCenter">
+                <div class="meiyiyetitle">{{title || innerCenterIndex.title}}</div>
+                <div>
+                    <slot name="btnArrCenter"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<style lang="scss" scoped>
+    .btnArr {
+        padding: 0 10px;
+    }
 
+    .meiyiyetitle {
+        display: flex;
+        align-items: center;
+    }
+
+    .btnInnerCenter {
+        border-bottom: 1px solid #d1d6e2;
+        padding: 16px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: rgb(255, 255, 255);
+    }
+</style>
 <script>
     export default {
-        name: "index"
+        name: "index",
+        props: {
+            title: {
+                type: String,
+                default: ""
+            },
+            innerCenterIndex: {
+                type: Object,
+                default: () => {
+                    return {
+                        title: ""
+                    }
+                }
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
