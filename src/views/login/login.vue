c5e3b97e2553c331c82c9cf8d5e4513989d0c4df
wangjiahao <1208200303@qq.com>
2020-12-21 11:01:06 +0800

    删除了动画

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -49,21 +49,13 @@
         </div>
       </div>
     </div>
-    <transition>
-      <Loading v-if="isLoading"></Loading>
-    </transition>
   </div>
 </template>
 
 <script>
 import { login, getFindWareOrg } from "@/api/api.js";
-import Loading from "@/components/loading/loading";
-import { reduceFun } from '../../utils/validate';
-
+import { reduceFun } from "../../utils/validate";
 export default {
-  components: {
-    Loading,
-  },
   data() {
     return {
       isLoading: false,
@@ -120,7 +112,7 @@ export default {
                 "X-Auth-wareName",
                 ok.data.result.user.wareFullName
               );
-                this.$router.push("/index");
+              this.$router.push("/index");
               //四个小时后清除用户名和密码
               setTimeout(() => {
                 this.$cookie.delete("userName");
@@ -140,7 +132,7 @@ export default {
                       label: v.orgName,
                     });
                   });
-                  orgArr = reduceFun(orgArr)
+                  orgArr = reduceFun(orgArr);
                   this.$store.dispatch("orgInforRequest", orgArr);
                 }
               });
